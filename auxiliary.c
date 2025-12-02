c2ea506b0c5239379b3f28b61779f5653aabd92f
Diogo Neves <neves.diogo98@gmail.com>
2025-12-02 13:58:56 +0000

    mapped and EEPROM operations

--- auxiliary.c
+++ auxiliary.c
@@ -1,5 +1,30 @@
 #include "auxiliary.h"
 
+void set_eeprom_default(){
+    /* From the prof example we should be able to just uncomment this and it should work for the default values
+    DATAEE_WriteByte(ADDR_PMON, 5);
+    DATAEE_WriteByte(ADDR_TALA, 3);
+    DATAEE_WriteByte(ADDR_TINA, 10);
+    DATAEE_WriteByte(ADDR_ALAF, 0);
+    DATAEE_WriteByte(ADDR_ALAH, 12);
+    DATAEE_WriteByte(ADDR_ALAM, 0);
+    DATAEE_WriteByte(ADDR_ALAS, 0);
+    DATAEE_WriteByte(ADDR_ALAT, 20);
+    DATAEE_WriteByte(ADDR_ALAL, 2);
+    DATAEE_WriteByte(ADDR_CLKH, 0);
+    DATAEE_WriteByte(ADDR_CLKM, 0);
+     */
+}
+
+void write_eeprom(uint16_t address, uint8_t value){
+    //DATAEE_WriteByte(ADDR_CLKM, value);
+}
+
+uint8_t read_eeprom(uint16_t address){
+    //return DATAEE_ReadByte(address)
+}
+
+
 unsigned char readTC74(void) {
     unsigned char value;
     do {
--- auxiliary.h
+++ auxiliary.h
@@ -10,6 +10,23 @@ typedef enum {time, time_alarm, temp_alarm, light_alarm, alarms, reset}ConfigSel
 typedef enum {sel_hours, sel_minutes, sel_seconds}TimeSelection;
 typedef enum {regular, activated}Activation;
 
+// Memory addresses
+#define EEPROM_START_ADDR 0x7000
+
+#define ADDR_PMON       (EEPROM_START_ADDR + 0) // 0x7000
+#define ADDR_TALA       (EEPROM_START_ADDR + 1) // 0x7001
+#define ADDR_TINA       (EEPROM_START_ADDR + 2) // 0x7002
+#define ADDR_ALAF       (EEPROM_START_ADDR + 3) // 0x7003
+#define ADDR_ALAH       (EEPROM_START_ADDR + 4) // 0x7004
+#define ADDR_ALAM       (EEPROM_START_ADDR + 5) // 0x7005
+#define ADDR_ALAS       (EEPROM_START_ADDR + 6) // 0x7006
+#define ADDR_ALAT       (EEPROM_START_ADDR + 7) // 0x7007
+#define ADDR_ALAL       (EEPROM_START_ADDR + 8) // 0x7008
+#define ADDR_CLKH       (EEPROM_START_ADDR + 9) // 0x7009
+#define ADDR_CLKM       (EEPROM_START_ADDR + 10) // 0x7010
+
+void set_eeprom_default();
+
 unsigned char readTC74 (void);
 
 
