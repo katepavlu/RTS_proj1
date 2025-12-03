5d7119182139bd1e7ec74350248c4a89c16965e1
Yigantopitekus <157696237+Yigantopitekus@users.noreply.github.com>
2025-12-03 21:00:12 +0100

    Update main.c

--- main.c
+++ main.c
@@ -66,7 +66,10 @@ volatile uint8_t light_alarm_trigd;
 
 
 #define PMON 5
-
+#define MINLUM  0x00
+#define MAXLUM  0x05
+#define MINTEMP 0x0A
+#define MAXTEMP 0x14
 void t1_isr() {
     static uint8_t xmilis;
     static uint8_t seconds_monitor;
@@ -97,12 +100,27 @@ void t1_isr() {
     btn_update(&S1, SW1_GetValue());
     btn_update(&S2, SW2_GetValue());
 }
+void t2_isr()
+{
+    TMR2_StopTimer();
+    PWM_Output_D4_Disable();
+}
+
 
 void main(void) {
     uint8_t temperature;
     uint8_t light_level = 2;
     char buf[17];
 
+    int minLum = 1023;
+    int maxLum = 0;
+    int minTemp = 255;
+    int maxTemp = 0;
+    int lumlvl;
+    int lumAlarm = 0;
+    uint8_t tstmp[3];
+    uint8_t newLum;
+    adc_result_t lum;
     // initialize the device
     SYSTEM_Initialize();
 
@@ -121,8 +139,11 @@ void main(void) {
     // Disable the Peripheral Interrupts
     //INTERRUPT_PeripheralInterruptDisable();
     TMR1_SetInterruptHandler(t1_isr);
-    // TMR2_SetInterruptHandler(t2_isr); maybe later on we have a 2nd timer for dealing with buttons?
-
+    TMR2_SetInterruptHandler(t2_isr); //maybe later on we have a 2nd timer for dealing with buttons?
+    ADCC_Initialize();
+    ADCC_DisableContinuousConversion();
+    
+    
     OpenI2C();
     //I2C_SCL = 1;
     //I2C_SDA = 1;
@@ -142,13 +163,81 @@ void main(void) {
     uint8_t alarms_enabled = 0;
 
     D5_SetHigh();
-
+    
     while (1) {
 
         // Periodic reading of temperature sensor
         if (sensors_read == 1) {
             temperature = readTC74();
+            lum = ADCC_GetSingleConversion(POT);
+
             sensors_read = 0;
+            
+            if(lum < 255){
+                lumlvl = 0;      
+                PWM_Output_D4_Enable();
+                TMR2_StartTimer();
+                PWM6_LoadDutyValue(1);
+                D2_SetHigh();
+            }
+            else if (lum > 255 && lum < 510){
+                lumlvl = 1;
+                PWM_Output_D4_Enable();
+                TMR2_StartTimer();
+                PWM6_LoadDutyValue(1);
+                D2_SetHigh();
+            }
+            else if (lum > 510 && lum < 765){
+                lumlvl = 2;
+                D2_SetLow();
+            }
+            else{
+                lumlvl = 3;
+                D2_SetLow();
+            }
+            
+            if(temperature > 20){
+                PWM_Output_D4_Enable();
+                TMR2_StartTimer();
+                PWM6_LoadDutyValue(1);
+                D3_SetHigh();
+            } else {
+                D3_SetLow();
+            }
+
+            newLum = (lum * 255) / 1023;
+
+            if(lum < minLum){
+                minLum = lum;
+                DATAEE_WriteByte(MINLUM,   hours);
+                DATAEE_WriteByte(MINLUM+1, minutes);
+                DATAEE_WriteByte(MINLUM+2, seconds);
+                DATAEE_WriteByte(MINLUM+3, newLum);
+                DATAEE_WriteByte(MINLUM+4, temperature);
+            }
+            if(lum > maxLum){
+                maxLum = lum;
+                DATAEE_WriteByte(MAXLUM,   hours);
+                DATAEE_WriteByte(MAXLUM+1, minutes);
+                DATAEE_WriteByte(MAXLUM+2, seconds);
+                DATAEE_WriteByte(MAXLUM+3, newLum);
+                DATAEE_WriteByte(MAXLUM+4, temperature);
+            }
+
+            if(temperature < minTemp){
+                minTemp = temp;
+                DATAEE_WriteByte(MINTEMP,   hours);
+                DATAEE_WriteByte(MINTEMP+1, minutes);
+                DATAEE_WriteByte(MINTEMP+2, seconds);
+                DATAEE_WriteByte(MINTEMP+3, temperature);
+            }
+            if(temperature > maxTemp){
+                maxTemp = temp;
+                DATAEE_WriteByte(MAXTEMP,   hours);
+                DATAEE_WriteByte(MAXTEMP+1, minutes);
+                DATAEE_WriteByte(MAXTEMP+2, seconds);
+                DATAEE_WriteByte(MAXTEMP+3, temperature);
+            }
             // max min stuff goes here later?
         }
         
